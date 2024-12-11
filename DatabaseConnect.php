0ac9e1d41b4d1ea63c1611502f7d5ac3dec03542
Dantouma07 <tonemail@example.comclear>
2024-12-11 17:25:55 +0000

    Insertion des données dans une base de données

---
+++ DatabaseConnect.php
@@ -0,0 +1,66 @@
+<?php
+const MYSQL_HOST = 'localhost';
+const MYSQL_PORT = 3306;
+const MYSQL_NAME = 'parainage';
+const MYSQL_USER = 'root';
+const MYSQL_PASSWORD = '';
+
+
+try {
+    $mysqlClient = new PDO(
+        sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8', MYSQL_HOST, MYSQL_NAME, MYSQL_PORT),
+        MYSQL_USER,
+        MYSQL_PASSWORD
+    );
+    $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //Récupération des données du formulaire
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = $_POST['name'];
+        $motDePasse = password_hash($_POST['password'], PASSWORD_BCRYPT); // Hachage du mot de passe
+        $numeroTelephone = $_POST['phone'];
+        $annee = $_POST['year'];
+
+        // Gestion de la photo (conversion en données binaires)
+        if (isset($_FILES['photo']) && $_FILES['photo']['error'] === UPLOAD_ERR_OK) {
+            $photo = file_get_contents($_FILES['photo']['tmp_name']);
+        } else {
+            throw new Exception('Erreur lors du téléchargement de la photo.');
+        }
+
+        //Exécution de la requête SQL
+        $query = $mysqlClient->prepare(
+            'INSERT INTO users (nom, mot_de_passe, numero_telephone, annee, photo) 
+             VALUES (:name, :password, :phone, :year, :photo)'
+        );
+
+        $query->execute([
+            ':name' => $name,
+            ':password' => $motDePasse,
+            ':phone' => $numeroTelephone,
+            ':year' => $annee,
+            ':photo' => $photo,
+        ]);}
+
+        
+} catch (Exception $exception) {
+    die('Erreur : ' . $exception->getMessage());
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Success</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Merci!</h1>
+    <img src="images/ucad.jpeg" alt="Photo">
+    <p>Vos informations ont été correctement enregistrées!</p>
+    <a href="index.html" class="btn">Back to Home</a>
+</body>
+</html>
+
---
+++ sql/Creation_base.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 10 déc. 2024 à 19:27
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `parainage`
+--
+CREATE DATABASE IF NOT EXISTS `parainage`;
+USE `parainage`;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `numero_telephone` varchar(9) NOT NULL,
+  `annee` enum('1ère année (Filleul)','2ème année (Parrain)') NOT NULL,
+  `photo` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
